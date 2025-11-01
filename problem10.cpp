189e8c4cc2139fa12de83f8f2270c4b15a627170
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-11-01 16:38:09 +0100

    sum of all matrix values

---
+++ problem10.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+enum enMatrixD { ROWS=3, COLS=3 };
+
+short int randomNumber(short int From, short int To)
+{
+    return ((rand() % (To - From + 1)) + From);
+}
+
+void fillMatrixWithRandomValues(int matrix[ROWS][COLS], short int Rows, short int Cols)
+{
+    for (short int i = 0; i < Rows; i++)
+    {
+        for (short int j = 0; j < Cols; j++)
+            matrix[i][j] = randomNumber(1, 100);
+    }
+}
+
+int  calculateSumOfMatrixValues(int matrix[ROWS][COLS], short int Rows, short int Cols)
+{
+    int Sum = 0;
+
+    for (short int i = 0; i < Rows; i++)
+    {
+        for (short int j = 0; j < Cols; j++)
+            Sum += matrix[i][j];
+    }
+
+    return (Sum);
+}
+
+void printMatrix(int matrix[ROWS][COLS], short int Rows, short int Cols)
+{
+    std::cout << "The Following is a " << Rows << "x" << Cols << " Matrix : \n";
+
+    for (short int i = 0; i < Rows; i++)
+    {
+        for (short int j = 0; j < Cols; j++)
+            std::cout << std::setw(3) << matrix[i][j] << "      ";
+        std::cout << std::endl;
+    }
+}
+
+int main(void)
+{
+    srand((unsigned int)time(NULL));
+    int matrix[COLS][ROWS];
+
+    fillMatrixWithRandomValues(matrix, ROWS, COLS);
+    printMatrix(matrix, ROWS, COLS);
+
+    return (0);
+}
