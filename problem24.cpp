7fe86e8a807e4b1bef0b04112aa4b7e2acdd1c19
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-11-03 15:14:44 +0100

    uper

---
+++ problem24.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+void printFirstCharacterOfEachWordIn(std::string &msg)
+{
+    bool isFirstLetter = true;
+
+    for (std::string::iterator iter = msg.begin(); iter < msg.end(); iter++)
+    {
+        if (*iter != ' ' && isFirstLetter)
+        {
+            if (islower(*iter))
+                *iter = toupper(*iter);
+            isFirstLetter = false;
+        }
+        
+        if (*iter == ' ')
+            isFirstLetter = true;
+    }
+}
+
+int main(void)
+{
+    std::string msg = "  Hello     World 2025 @you have 2 months to end ";
+
+    printFirstCharacterOfEachWordIn(msg);
+
+    std::cout << msg << std::endl;
+
+    return (0);
+}
