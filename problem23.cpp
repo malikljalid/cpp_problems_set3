65e01b7328683f0445e8ef96df2392c8b1de8a10
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-11-03 12:28:18 +0100

    print first character of each world in a string

---
+++ problem23.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void printFirstCharacterOfEachWordIn(std::string msg)
+{
+    std::string::iterator iter;
+    bool isFirstLetter = true;
+
+    for (iter = msg.begin(); iter < msg.end(); iter++)
+    {
+        if (isFirstLetter && *iter != ' ')
+        {
+            std::cout << *iter << std::endl;
+            isFirstLetter = false;
+        }
+        if (*iter == ' ')
+            isFirstLetter = true;
+    }
+}
+
+int main(void)
+{
+    printFirstCharacterOfEachWordIn("Hello     World 2025 you have 2 months to end");
+
+    return (0);
+}
