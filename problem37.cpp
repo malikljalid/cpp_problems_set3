4473f415f22b5f2edd0e3fc067d1a76eb5e72e24
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-11-04 20:01:45 +0100

    split string to a vector

---
+++ problem37.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector <std::string> split(std::string msg)
+{
+    int                         end   = (int)msg.length();
+    int                         j = 0;
+    std::vector <std::string>   vWords;
+
+    for (int i = 0; i < end; i++)
+    {
+        j = i;
+        while (msg[i] != ' ' && i < end)
+        {
+            if (msg[i+1] == ' ')
+                vWords.push_back(msg.substr(j, i - j + 1));
+            i++;
+        }
+    }
+
+    return (vWords);
+}
+
+int main(void)
+{
+    std::string msg = "   Hello world,    hozza going  !!  ";
+
+    std::vector <std::string> words = split(msg);
+
+    for (std::string & i : words)
+    {
+        std::cout << i << std::endl;
+    }
+
+    return (0);
+}
